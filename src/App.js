fb472a6b0eea4d8e70667df472ebbc824106238a
fekstr <frithiof.ekstrom@gmail.com>
2019-10-02 21:57:09 +0200

    Added basic routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Test heading</h1>
-    </div>
+    <Container>
+      <Switch>
+        <Route path='/city/:id' component={CityData} />
+        <Route path='/city' component={CitySearch} />
+        <Route path='/country/:id' component={CountryData} />
+        <Route path='/country' component={CountrySearch} />
+        <Route path='/not-found' component={NotFound} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </Container>
   );
 }
 
